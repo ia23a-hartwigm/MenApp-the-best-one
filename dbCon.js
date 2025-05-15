52a73173b09acef10483aebc8fe4c23d91409f6a
IA23a-burkartj <burkartj@bzz.ch>
2025-05-15 10:50:20 +0200

    db_connection and sql statements

--- dbCon.js
+++ dbCon.js
@@ -2,11 +2,11 @@ const mysql = require('mysql2/promise');
 
 // Create a connection pool for better performance and connection management
 const pool = mysql.createPool({
-    host: '127.0.0.1',
+    host: '127.0.0.1',             // local because of SSH tunnel or local execution
     port: '3306',
-    user: 'root',
-    password: 'skillsbattle',
-    database: 'labyrinth',
+    user: 'm426menapp',       // your Froxlor MySQL user
+    password: 'wGdYnsytgm', // leave this field for you to fill
+    database: 'm426menappsql1',   // usually same as user unless you've created more
     waitForConnections: true,
     connectionLimit: 10,
     queueLimit: 0
